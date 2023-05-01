6febe76df6a219cfa3dd6a927312bdad52507172
Nikita Nikiforov <111698816+NixNi@users.noreply.github.com>
2023-05-01 21:24:44 +0300

    feat: add for CapsLock and Shift buttons

--- src/Key.js
+++ src/Key.js
@@ -14,59 +14,102 @@ function insert(TextHolder, text) {
   TextHolder.setSelectionRange(cursorPos + 1, cursorPos + 1);
 }
 
-
-export default class Key {
-  constructor(TextHolder, Upper, Down, width, code, fun, control = 0) {
+class Key {
+  constructor(TextHolder, Keyboard, Upper, Down, width, code, fun, sh = 0, aUpper, aDown) {
     this.TextHolder = TextHolder;
+    this.Keyboard = Keyboard;
     this.Upper = Upper;
     this.Down = Down;
+    this.aUpper = aUpper;
+    this.aDown = aDown;
     this.fun = fun;
-    this.control = control;
     this.width = width;
+    this.sh = sh;
     this.element = document.createElement('div');
     this.element.classList.add("key");
     this.element.style.width = width;
     this.element.innerText = Down;
     this.element.addEventListener("mousedown", () => {
-      TextHolder.focus()
-      let evt = new KeyboardEvent('keydown', { 'code': code});
+      TextHolder.focus();
+      let evt = new KeyboardEvent('keydown', { 'code': code });
       document.dispatchEvent(evt);
-      let evtInp = new InputEvent({inputType: "insertText", data: Upper })
-      TextHolder.dispatchEvent(evtInp)
-      if (!fun) insert(TextHolder, Down)
-      else fun();
+      if (!fun) {
+        if (Keyboard.control == 0) insert(TextHolder, Down);
+        if (Keyboard.control == 1) insert(TextHolder, Upper);
+        if (Keyboard.control == 2) insert(TextHolder, !sh ? Upper : Down);
+        if (Keyboard.control == 3) insert(TextHolder, sh ? Upper : Down);
+      }
+      else fun(this.Keyboard.control, this.Keyboard.setControl.bind(Keyboard));
     });
     this.element.addEventListener("mouseup", () => {
-      TextHolder.focus()
+      TextHolder.focus();
       let evt = new KeyboardEvent('keyup', { 'code': code });
       document.dispatchEvent(evt);
     });
     this.element.addEventListener("mouseleave", () => {
-      let evt = new KeyboardEvent('keyup', { 'code': code});
+      let evt = new KeyboardEvent('keyup', { 'code': code });
       document.dispatchEvent(evt);
     });
   }
   getElement() {
     return this.element;
   }
-  setUp() {
-    this.element.innerText = this.Upper;
+  changeLanguage() {
+    let i = this.Down;
+    this.Down = this.aDown;
+    this.aDown = i;
+    i = this.Upper;
+    this.Upper = this.aUpper;
+    this.aUpper = i;
+    this.setControl();
   }
-  setDown() {
-    this.element.innerText = this.Down;
-  }
-  setUpCaps() {
-    this.element.innerText = this.Upper;
-  }
-  setDownCaps() {
-    this.element.innerText = this.Down;
-  }
-
-  setControl(c) {
-    this.control = c;
-    if (c == 0) this.setDown();
-    if (c == 1) this.setUp();
-    if (c == 2) this.setUpCaps();
-    if (c == 3) this.setDownCaps();
+  setControl() {
+    if (this.Keyboard.control == 0) this.element.innerText = this.Down;
+    if (this.Keyboard.control == 1) this.element.innerText = this.Upper;
+    if (this.Keyboard.control == 2) this.element.innerText = !this.sh ? this.Upper : this.Down;
+    if (this.Keyboard.control == 3) this.element.innerText = this.sh ? this.Upper : this.Down;
   }
 }
+
+
+class Keyboard {
+  constructor(Keys, TextHolder, control = 0) {
+    this.control = control;
+    this.Keys = [];
+    this.KeysI = {};
+    for (let k of Keys) {
+      let elem = new Key(TextHolder, this, ...k,);
+      this.Keys.push(elem);
+      this.KeysI[k[3]] = elem;
+    }
+  }
+  getElements() {
+    return this.Keys.map(it => it.getElement());
+  }
+  getElementsI() {
+    return this.KeysI;
+  }
+  setControl(num) {
+    this.control = num;
+    for (let i of this.Keys) i.setControl();
+  }
+  initEventListeners() {
+    document.addEventListener("keydown", (e) => {
+      if ((e.code == "ShiftLeft" || e.code == "ShiftRight") && !e.getModifierState('CapsLock') && e.isTrusted) this.setControl(1);
+      if (e.code == "CapsLock" && e.getModifierState('CapsLock') && e.isTrusted) this.setControl(2);
+      if (e.code == "CapsLock" && !e.getModifierState('CapsLock') && e.isTrusted) this.setControl(0);
+      if ((e.code == "ShiftLeft" || e.code == "ShiftRight") && e.getModifierState('CapsLock') && e.isTrusted) this.setControl(3);
+      this.KeysI[e.code]?.getElement().classList.add("active");
+    });
+    document.addEventListener("keyup", (e) => {
+      if ((e.code == "ShiftLeft" || e.code == "ShiftRight") && !e.getModifierState('CapsLock') && e.isTrusted) this.setControl(0);
+      if ((e.code == "ShiftLeft" || e.code == "ShiftRight") && e.getModifierState('CapsLock') && e.isTrusted) this.setControl(2);
+      if (!(e.code == "ShiftLeft" && (this.control == 1 || this.control == 3)
+        || e.code == "ShiftRight" && (this.control == 1 || this.control == 3)
+        || e.code == "CapsLock" && (this.control == 2 || this.control == 3)))
+        this.KeysI[e.code]?.getElement().classList.remove("active");
+    });
+  }
+}
+
+export default Keyboard;
--- src/keysNum.js
+++ src/keysNum.js
@@ -48,14 +48,22 @@ export default function KeysNumInit(TextHolder) {
       TextHolder.setSelectionRange(cursorPos, cursorPos);
     }
   }
-  function caps() {
-
+  function caps(Kb,Kbs) {
+    console.log(Kb);
+    if (Kb == 0) Kbs(2);
+    if (Kb == 1) Kbs(3);
+    if (Kb == 2) Kbs(0);
+    if (Kb == 3) Kbs(1);
   }
   function enter() {
     insert("\n");
   }
-  function shift() {
-
+  function shift(Kb, Kbs) {
+    console.log(Kb);
+    if (Kb == 0) Kbs(1);
+    if (Kb == 1) Kbs(0);
+    if (Kb == 2) Kbs(3);
+    if (Kb == 3) Kbs(2);
   }
 
   function ctrl() {
@@ -71,93 +79,93 @@ export default function KeysNumInit(TextHolder) {
     const cursorPos = TextHolder.selectionStart;
     const currentLineStartPosition = TextHolder.value.lastIndexOf('\n', cursorPos - 1);
     const previousLineStartPosition = TextHolder.value.lastIndexOf('\n', currentLineStartPosition - 1);
-    const newCursorPos = Math.min(currentLineStartPosition, previousLineStartPosition + cursorPos - currentLineStartPosition ) 
+    const newCursorPos = Math.min(currentLineStartPosition, previousLineStartPosition + cursorPos - currentLineStartPosition);
     TextHolder.setSelectionRange(newCursorPos, newCursorPos);
   }
   function arrowDown() {
     const cursorPos = TextHolder.selectionStart;
     const currentLineStartPosition = TextHolder.value.lastIndexOf('\n', cursorPos - 1);
     const currentLineEndPosition = TextHolder.value.indexOf('\n', cursorPos);
-    const nextLineStartPosition = currentLineEndPosition+1
-    const nextLineEndPosition = TextHolder.value.indexOf('\n', nextLineStartPosition) != -1 ? TextHolder.value.indexOf('\n', nextLineStartPosition) : TextHolder.value.length
+    const nextLineStartPosition = currentLineEndPosition + 1;
+    const nextLineEndPosition = TextHolder.value.indexOf('\n', nextLineStartPosition) != -1 ? TextHolder.value.indexOf('\n', nextLineStartPosition) : TextHolder.value.length;
     const newCursorPos = Math.min(nextLineEndPosition, nextLineStartPosition + cursorPos - currentLineStartPosition - 1);
     TextHolder.setSelectionRange(newCursorPos, newCursorPos);
   }
   function arrowLeft() {
     const cursorPos = TextHolder.selectionStart;
-    TextHolder.setSelectionRange(cursorPos-1, cursorPos-1)
+    TextHolder.setSelectionRange(cursorPos - 1, cursorPos - 1);
   }
   function arrowRight() {
     const cursorPos = TextHolder.selectionStart;
-    TextHolder.setSelectionRange(cursorPos+1, cursorPos+1)
+    TextHolder.setSelectionRange(cursorPos + 1, cursorPos + 1);
   }
 
   const KeysNum = [
-    ["~", "`", "6%", "Backquote", undefined],
-    ["!", "1", "6%", "Digit1", undefined],
-    ["@", "2", "6%", "Digit2", undefined],
-    ["#", "3", "6%", "Digit3", undefined],
-    ["$", "4", "6%", "Digit4", undefined],
-    ["%", "5", "6%", "Digit5", undefined],
-    ["^", "6", "6%", "Digit6", undefined],
-    ["&", "7", "6%", "Digit7", undefined],
-    ["*", "8", "6%", "Digit8", undefined],
-    ["(", "9", "6%", "Digit9", undefined],
-    [")", "0", "6%", "Digit0", undefined],
-    ["_", "-", "6%", "Minus", undefined],
-    ["+", "=", "6%", "Equal", undefined],
-    ["Backspace", "Backspace", "12%", "Backspace", backspace],
-    ["Tab", "Tab", "6%", "Tab", tab],
-    ["Q", "q", "6%", "KeyQ", undefined],
-    ["W", "w", "6%", "KeyW", undefined],
-    ["E", "e", "6%", "KeyE", undefined],
-    ["R", "r", "6%", "KeyR", undefined],
-    ["T", "t", "6%", "KeyT", undefined],
-    ["Y", "y", "6%", "KeyY", undefined],
-    ["U", "u", "6%", "KeyU", undefined],
-    ["I", "i", "6%", "KeyI", undefined],
-    ["O", "o", "6%", "KeyO", undefined],
-    ["P", "p", "6%", "KeyP", undefined],
-    ["{", "[", "6%", "BracketLeft", undefined],
-    ["}", "]", "6%", "BracketRight", undefined],
-    ["/", "\\", "6%", "Backslash", undefined],
-    ["DEL", "DEL", "6%", "Delete", del],
-    ["Caps Lock", "Caps Lock", "12%", "CapsLock", caps],
-    ["A", "a", "6%", "KeyA", undefined],
-    ["S", "s", "6%", "KeyS", undefined],
-    ["D", "d", "6%", "KeyD", undefined],
-    ["F", "f", "6%", "KeyF", undefined],
-    ["G", "g", "6%", "KeyG", undefined],
-    ["H", "h", "6%", "KeyH", undefined],
-    ["J", "j", "6%", "KeyJ", undefined],
-    ["K", "k", "6%", "KeyK", undefined],
-    ["L", "l", "6%", "KeyL", undefined],
-    [":", ";", "6%", "Semicolon", undefined],
-    ["\"", "'", "6%", "Quote", undefined],
-    ["Enter", "Enter", "12%", "Enter", enter],
-    ["Shift", "Shift", "12%", "ShiftLeft", shift],
-    ["|", "\\", "6%", "IntlBackslash", undefined],
-    ["Z", "z", "6%", "KeyZ", undefined],
-    ["X", "x", "6%", "KeyX", undefined],
-    ["C", "c", "6%", "KeyC", undefined],
-    ["V", "v", "6%", "KeyV", undefined],
-    ["B", "b", "6%", "KeyB", undefined],
-    ["N", "n", "6%", "KeyN", undefined],
-    ["M", "m", "6%", "KeyM", undefined],
-    ["<", ",", "6%", "Comma", undefined],
-    [">", ".", "6%", "Period", undefined],
-    ["?", "/", "6%", "Slash", undefined],
-    ["↑", "↑", "6%", "ArrowUp", arrowUp],
-    ["Shift", "Shift", "6%", "ShiftRight", shift],
-    ["Ctrl", "Ctrl", "9%", "ControlLeft", ctrl],
-    ["Win", "Win", "6%", "MetaLeft", meta],
-    ["Alt", "Alt", "6%", "AltLeft", alt],
-    [" ", " ", "40%", "Space", undefined],
-    ["Alt", "Alt", "6%", "AltRight", alt],
-    ["Ctrl", "Ctrl", "9%", "ControlRight", ctrl],
-    ["←", "←", "6%", "ArrowLeft", arrowLeft],
-    ["↓", "↓", "6%", "ArrowDown", arrowDown],
-    ["→", "→", "6%", "ArrowRight", arrowRight],
+    ["~", "`", "6%", "Backquote", undefined, 0],
+    ["!", "1", "6%", "Digit1", undefined, 1],
+    ["@", "2", "6%", "Digit2", undefined, 1],
+    ["#", "3", "6%", "Digit3", undefined, 1],
+    ["$", "4", "6%", "Digit4", undefined, 1],
+    ["%", "5", "6%", "Digit5", undefined, 1],
+    ["^", "6", "6%", "Digit6", undefined, 1],
+    ["&", "7", "6%", "Digit7", undefined, 1],
+    ["*", "8", "6%", "Digit8", undefined, 1],
+    ["(", "9", "6%", "Digit9", undefined, 1],
+    [")", "0", "6%", "Digit0", undefined, 1],
+    ["_", "-", "6%", "Minus", undefined, 1],
+    ["+", "=", "6%", "Equal", undefined, 1],
+    ["Backspace", "Backspace", "12%", "Backspace", backspace, 0],
+    ["Tab", "Tab", "6%", "Tab", tab, 0],
+    ["Q", "q", "6%", "KeyQ", undefined, 0],
+    ["W", "w", "6%", "KeyW", undefined, 0],
+    ["E", "e", "6%", "KeyE", undefined, 0],
+    ["R", "r", "6%", "KeyR", undefined, 0],
+    ["T", "t", "6%", "KeyT", undefined, 0],
+    ["Y", "y", "6%", "KeyY", undefined, 0],
+    ["U", "u", "6%", "KeyU", undefined, 0],
+    ["I", "i", "6%", "KeyI", undefined, 0],
+    ["O", "o", "6%", "KeyO", undefined, 0],
+    ["P", "p", "6%", "KeyP", undefined, 0],
+    ["{", "[", "6%", "BracketLeft", undefined, 0],
+    ["}", "]", "6%", "BracketRight", undefined, 0],
+    ["/", "\\", "6%", "Backslash", undefined, 0],
+    ["DEL", "DEL", "6%", "Delete", del, 0],
+    ["Caps Lock", "Caps Lock", "12%", "CapsLock", caps, 0],
+    ["A", "a", "6%", "KeyA", undefined, 0],
+    ["S", "s", "6%", "KeyS", undefined, 0],
+    ["D", "d", "6%", "KeyD", undefined, 0],
+    ["F", "f", "6%", "KeyF", undefined, 0],
+    ["G", "g", "6%", "KeyG", undefined, 0],
+    ["H", "h", "6%", "KeyH", undefined, 0],
+    ["J", "j", "6%", "KeyJ", undefined, 0],
+    ["K", "k", "6%", "KeyK", undefined, 0],
+    ["L", "l", "6%", "KeyL", undefined, 0],
+    [":", ";", "6%", "Semicolon", undefined, 0],
+    ["\"", "'", "6%", "Quote", undefined, 0],
+    ["Enter", "Enter", "12%", "Enter", enter, 0],
+    ["Shift", "Shift", "12%", "ShiftLeft", shift, 0],
+    ["|", "\\", "6%", "IntlBackslash", undefined, 0],
+    ["Z", "z", "6%", "KeyZ", undefined, 0],
+    ["X", "x", "6%", "KeyX", undefined, 0],
+    ["C", "c", "6%", "KeyC", undefined, 0],
+    ["V", "v", "6%", "KeyV", undefined, 0],
+    ["B", "b", "6%", "KeyB", undefined, 0],
+    ["N", "n", "6%", "KeyN", undefined, 0],
+    ["M", "m", "6%", "KeyM", undefined, 0],
+    ["<", ",", "6%", "Comma", undefined, 0],
+    [">", ".", "6%", "Period", undefined, 0],
+    ["?", "/", "6%", "Slash", undefined, 0],
+    ["↑", "↑", "6%", "ArrowUp", arrowUp, 0],
+    ["Shift", "Shift", "6%", "ShiftRight", shift, 0],
+    ["Ctrl", "Ctrl", "9%", "ControlLeft", ctrl, 0],
+    ["Win", "Win", "6%", "MetaLeft", meta, 0],
+    ["Alt", "Alt", "6%", "AltLeft", alt, 0],
+    [" ", " ", "40%", "Space", undefined, 0],
+    ["Alt", "Alt", "6%", "AltRight", alt, 0],
+    ["Ctrl", "Ctrl", "9%", "ControlRight", ctrl, 0],
+    ["←", "←", "6%", "ArrowLeft", arrowLeft, 0],
+    ["↓", "↓", "6%", "ArrowDown", arrowDown, 0],
+    ["→", "→", "6%", "ArrowRight", arrowRight, 0],
   ];
 
   return KeysNum;
